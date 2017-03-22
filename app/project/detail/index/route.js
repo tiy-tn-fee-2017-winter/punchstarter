28f3b9bc2e9905be8034d88ef4e20c180b67ce86
Ryan Tablada <ryan.tablada@gmail.com>
2017-03-22 13:47:34 -0500

    Load data for project.detail.index

--- app/project/detail/index/route.js
+++ app/project/detail/index/route.js
@@ -1,4 +1,12 @@
 import Ember from 'ember';
+import config from 'punchstarter/config/environment';
 
 export default Ember.Route.extend({
+  model() {
+    // Request part of the URL from 'project.detail' route
+    const id = this.paramsFor('project.detail').id;
+
+    return fetch(`${config.apiUrl}/projects/${id}`)
+      .then(r => r.json());
+  }
 });
