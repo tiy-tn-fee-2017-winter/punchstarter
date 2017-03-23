6e4e8334451004e377293bd9d1c2c0bf620de8cc
Ryan Tablada <ryan.tablada@gmail.com>
2017-03-23 10:49:56 -0500

    Clear form after saving

--- app/project/new/controller.js
+++ app/project/new/controller.js
@@ -14,6 +14,8 @@ export default Ember.Controller.extend({
       const project = this.store.createRecord('project', this.formValues);
 
       project.save().then((project) => {
+        this.set('formValues', {});
+
           // Redirect to index route
           this.transitionToRoute('project.detail', project.id);
           // this.$router.push({ name: 'project.detail', params: { id: project.id } });
