7ab249a77b9fb259d252af6987fc7209af878102
Ryan Tablada <ryan.tablada@gmail.com>
2017-03-23 10:47:13 -0500

    create and find project

--- app/project/detail/route.js
+++ app/project/detail/route.js
@@ -1,11 +1,9 @@
 import Ember from 'ember';
-import config from 'punchstarter/config/environment';
 
 export default Ember.Route.extend({
   model(params) {
     const id = params.id;
 
-    return fetch(`${config.apiUrl}/projects/${id}`)
-      .then(r => r.json());
+    return this.store.findRecord('project', id);
   }
 });
--- app/project/new/controller.js
+++ app/project/new/controller.js
@@ -1,5 +1,4 @@
 import Ember from 'ember';
-const apiUrl = 'https://punchstarter-api.herokuapp.com/ryan/basic/projects';
 
 export default Ember.Controller.extend({
   formValues: {
@@ -11,13 +10,10 @@ export default Ember.Controller.extend({
   },
 
   actions: {
-    save() {
-      fetch(apiUrl, {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json', accept: 'application/json' },
-        body: JSON.stringify(this.formValues),
-      }).then(r => r.json())
-        .then((project) => {
+    submitForm() {
+      const project = this.store.createRecord('project', this.formValues);
+
+      project.save().then((project) => {
           // Redirect to index route
           this.transitionToRoute('project.detail', project.id);
           // this.$router.push({ name: 'project.detail', params: { id: project.id } });
