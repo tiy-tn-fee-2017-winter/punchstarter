cae83a968d0e90b49b545b618433bfe9c8211a6a
Ryan Tablada <ryan.tablada@gmail.com>
2017-03-28 11:06:49 -0500

    Start using Ember Changeset

--- app/contact/edit/controller.js
+++ app/contact/edit/controller.js
@@ -1,4 +1,16 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  actions: {
+    async saveContact(changeset) {
+      await changeset.save();
+
+      this.transitionToRoute('contact.index');
+    },
+    // saveContact(changeset) {
+    //   changeset.save().then(() => {
+    //     this.transitionToRoute('contact.index');
+    //   });
+    // },
+  }
 });
