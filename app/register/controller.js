8a0fa585cf772617d3f35f99a97e07625baea494
Ryan Tablada <ryan.tablada@gmail.com>
2017-03-29 11:59:14 -0500

    feat: save user from form

--- app/register/controller.js
+++ app/register/controller.js
@@ -1,4 +1,18 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+
+  actions: {
+    async saveUser(changeset) {
+      changeset.save();
+
+      const user = this.store.createRecord('user', this.model);
+
+      await user.save();
+
+      alert('Account Created');
+
+      this.transitionToRoute('index');
+    }
+  }
 });
